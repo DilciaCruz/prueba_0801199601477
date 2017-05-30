1f266025454b9424da32447e8b97ace0545d8466
Unknown <dilcia_salgado1995@hotmail.com>
2017-05-29 19:34:02 -0600

    Agregar get para jsons
    Agregar metodo get y post

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Prueba de Seminario de Software' });
 });
+
+router.get('/informacion', function(req,res,next){
+  res.render('informaciongeneral',{"txtmensaje":""});
+});
+
+var mensaje = [];
+
+router.post('/informacion',function(req,res,next){
+  console.log(req.body);
+    mensaje.push(req.body.txtmensaje.concat(" Prueba"));
+    var msgs = mensaje;
+    var rtObject = {}; // {"txtEmail": req.body.txtEmail, "msg":msgs};
+    rtObject.txtmensaje = req.body.txtmensaje;
+    rtObject.msg= msgs;
+  res.render('informaciongeneral', rtObject);
+});
+
+router.get('/datosgenerales', function(req,res,next){
+  var datos = [
+                {"Nombre":"Dilcia","Apellido":"Cruz","Identidad":"0801199601477","Telefono":"32647890","Correo":"drosycruz@gmail.com"}
+              ];
+  res.json(datos);
+
+});
+
+
 
 module.exports = router;
